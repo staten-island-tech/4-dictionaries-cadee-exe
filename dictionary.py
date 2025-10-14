54af0731f178bb01ca4a986cd6665b2ba729221a
cadee-exe <cadeez@nycstudents.net>
2025-10-14 06:26:16 -0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -5,7 +5,7 @@ item = {
     "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
 }
 dict_australia = {"name" : "continent, country","price":99999999999}
-def best_buy_items():
-    best_buy_items[0]["australia"]
-    best_buy_items[0]["99999999999"]
+print("best_buy_items")
+"best_buy_items"[0]["australia"]
+"best_buy_items"[0]["99999999999"]
     
