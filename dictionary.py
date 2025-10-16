c5e7685d1f6765b681a399aea4b81b9cf91b6d6b
cadee-exe <cadeez@nycstudents.net>
2025-10-16 06:12:25 -0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -5,7 +5,17 @@ item = {
     "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
 }
 dict_australia = {"name" : "continent, country","price":99999999999}
+dict_penguin = {"name" : "animal, pet","price":120}
 print("best_buy_items")
-"best_buy_items"[0]["australia"]
-"best_buy_items"[0]["99999999999"]
-    
+"best_buy_items"["australia, penguin"]
+"best_buy_items]"["99999999999, 120"]
+
+student = {'name': 'Bob', 'school': 'computing', 'grades':(66, 78, 99)}
+
+
+best_buy_items = [
+    {
+        "name": "Australia\"Very dangerous continent"
+        
+    }
+]
