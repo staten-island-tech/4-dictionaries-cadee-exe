72403dface7455103986550129ea3e4a8bbe33b9
cadee-exe <cadeez@nycstudents.net>
2025-10-17 06:26:19 -0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -3,19 +3,20 @@ item = {
     "price": 429.99,
     "department": "Televisions",
     "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+
+        "name": "Australia\" Continent",
+        "price": 99999999999,
+        "department": "Continents",
+        "description": "Very dangerous continent"
+
+        "name": "L'aqua water\" from France",
+        "price": 55.99,
+        "department": "Fancy water for the rich",
+    "   description": "If you don't buy you are poor"
 }
-dict_australia = {"name" : "continent, country","price":99999999999}
-dict_penguin = {"name" : "animal, pet","price":120}
-print("best_buy_items")
-"best_buy_items"["australia, penguin"]
-"best_buy_items]"["99999999999, 120"]
 
-student = {'name': 'Bob', 'school': 'computing', 'grades':(66, 78, 99)}
+for index, item in enumerate(item):
+    print(index, ":", item["name"])
 
 
-best_buy_items = [
-    {
-        "name": "Australia\"Very dangerous continent"
-        
-    }
-]
+
