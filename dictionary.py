55668735dc84c8dbd26f674e5d92d70d430754f3
cadee-exe <cadeez@nycstudents.net>
2025-10-21 06:26:20 -0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,22 +1,22 @@
-item = {
+best_buy_items = { 
     "name": "Samsung 55\" 4K UHD TV",
     "price": 429.99,
     "department": "Televisions",
-    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps.",
 
-        "name": "Australia\" Continent",
-        "price": 99999999999,
-        "department": "Continents",
-        "description": "Very dangerous continent"
+    "name": "French water\" Fancy water",
+    "price": 555.99,
+    "department": "Water",
+    "description": "Water for the rich.",
 
-        "name": "L'aqua water\" from France",
-        "price": 55.99,
-        "department": "Fancy water for the rich",
-    "   description": "If you don't buy you are poor"
+    "name": "China\" Country",
 }
+for index, item in enumerate(best_buy_items):
+    print(index, ":" ,best_buy_items["name"])
+best_buy_items[0]["name"]
+    
 
-for index, item in enumerate(item):
-    print(index, ":", item["name"])
 
+    
 
 
