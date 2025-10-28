14c472b9fe3e58ae9b720488c14dcdd06af21fe2
cadee-exe <cadeez@nycstudents.net>
2025-10-28 09:25:49 -0400

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,27 +1,30 @@
 best_buy_items = {
-{"name": "Samsung 55\" 4K UHD TV",
-    "price": 429.99,
-    "department": "Televisions",
-    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."},
+    
+        "name": "Samsung 55\" 4K UHD TV",
+        "price": 429.99,
+        "department": "Televisions",
+        "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps.",
 
-{"name": "French water\" Fancy water",
-    "price": 555.99,
-    "department": "Water",
-    "description": "Water for the rich.",},
+        "name": "French water\" Fancy water",
+        "price": 555.99,
+        "department": "Water",
+        "description": "Water for the rich.",
 
-{"name": "China\" Country",
-    "price": 6767676767,
-    "department": "Densely populated country",
-    "description": "Perfect for exploding"},
+        "name": "China\" Country",
+        "price": 77777777777.99,
+        "department": "Densely populated country",
+        "description": "",
 
-{"name": "Pig\" Food",
-    "price": 6.45,
-    "department": "Good food",
-    "description": "Make the vegans mad"}
+        "name": "Pig\" Food",
+        "price": 6.45,
+        "department": "Good food",
+        "description": "Make the vegans mad"
 }
 for index, item in enumerate(best_buy_items):
-            print(index, ":" ,best_buy_items)["price"]
-best_buy_items[0]["price"]
+            print(index, ":" ,(best_buy_items)["name"])
+            best_buy_items[3]["name"],
+
+
 
 
 
