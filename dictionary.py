2bb878fb3eaad51d2f9c7640eece1570ec3895a8
cadee-exe <cadeez@nycstudents.net>
2025-10-28 09:08:09 -0400

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,19 +1,30 @@
-best_buy_items = { 
-    "name": "Samsung 55\" 4K UHD TV",
+best_buy_items = {
+{"name": "Samsung 55\" 4K UHD TV",
     "price": 429.99,
     "department": "Televisions",
-    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps.",
+    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."},
 
-    "name": "French water\" Fancy water",
+{"name": "French water\" Fancy water",
     "price": 555.99,
     "department": "Water",
-    "description": "Water for the rich.",
+    "description": "Water for the rich.",},
 
-    "name": "China\" Country",
+{"name": "China\" Country",
+    "price": 6767676767,
+    "department": "Densely populated country",
+    "description": "Perfect for exploding"},
+
+{"name": "Pig\" Food",
+    "price": 6.45,
+    "department": "Good food",
+    "description": "Make the vegans mad"}
 }
 for index, item in enumerate(best_buy_items):
-    print(index, ":" ,best_buy_items["name"])
-best_buy_items[0]["name"]
+            print(index, ":" ,best_buy_items)["price"]
+best_buy_items[0]["price"]
+
+
+
     
 
 
