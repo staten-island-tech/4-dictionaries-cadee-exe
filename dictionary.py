e6d4395470055d5649aa151ee12c59c0630be6ad
cadee-exe <cadeez@nycstudents.net>
2025-10-29 09:25:32 -0400

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,36 +1,40 @@
-best_buy_items = {
+print("Welcome to the shop! Buy whatever you like.")
+best_buy_items = [
     
-        "name": "Samsung 55\" 4K UHD TV",
+        {
+                "name": "Samsung 55"" 4K UHD TV",
         "price": 429.99,
         "department": "Televisions",
-        "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps.",
+        "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."},
 
-        "name": "French water\" Fancy water",
+        {
+                "name": "French water"" Fancy water",
         "price": 555.99,
         "department": "Water",
-        "description": "Water for the rich.",
+        "description": "Water for the rich."},
 
-        "name": "China\" Country",
+        {
+                "name": "China"" Country",
         "price": 77777777777.99,
-        "department": "Densely populated country",
-        "description": "",
+        "department": "",
+        "description": "Densely populated country"},
 
-        "name": "Pig\" Food",
+        {
+            "name": "Baconetta"" Food",
         "price": 6.45,
         "department": "Good food",
-        "description": "Make the vegans mad"
-}
+        "description": "Make the vegans mad"},
+
+      
+
+]
 for index, item in enumerate(best_buy_items):
-            print(index, ":" ,(best_buy_items)["name"])
-            best_buy_items[3]["name"],
+            print(index, ":" ,(item)["name"])
+            best_buy_items[0]["name"]
 
-
-
-
-
-    
-
-
-    
-
-
+choice = input("Buy whatever you like!")
+cart = []
+cart.append(choice)
+print(f"Added", choice, "to cart")
+while cart != "done":
+        
